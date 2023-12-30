6ca3bf4b1b20c6d0ad59156db8b7dc898f96e650
Juan Pablo Formento <juan_pablo94@outlook.com>
2023-12-30 13:44:01 +0100

    ejercicio 2 iniciado

--- Nivel 1/3-Condicionales2/ejercicio2/Program.cs
+++ Nivel 1/3-Condicionales2/ejercicio2/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+ * Un importante negocio de desinfectante líquido realiza descuentos dependiendo de la cantidad de litros 
+ * vendidos según la siguiente escala:
+
+    Si vende menos de 100 litros, no hay descuento.
+    Si vende entre 101 y 300 litros, el descuento es del 10%.
+    Si vende entre 301 y 500 litros, el descuento es del 15%.
+    Finalmente, si la venta es de más de 500 litros, el descuento es del 25%.
+
+   Hacer un programa que solicite el ingreso del importe total de la venta y la cantidad de 
+   litros vendidos y calcule y emita el importe con el descuento  aplicado..
+ */
+
+float litros, importe;
+
+Console.WriteLine("Ingrese litros vendidos");
+litros = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese importe de venta");
+importe = int.Parse(Console.ReadLine());
+
+
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Probando git!");
