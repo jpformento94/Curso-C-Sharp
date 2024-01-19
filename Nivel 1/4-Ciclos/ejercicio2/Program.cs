40e7970b3682c9ca8da97584879e1031df9b2866
Juan Pablo Formento <juan_pablo94@outlook.com>
2024-01-19 15:28:48 +0100

    ejercicio 2 terminado

--- Nivel 1/4-Ciclos/ejercicio2/Program.cs
+++ Nivel 1/4-Ciclos/ejercicio2/Program.cs
@@ -3,3 +3,25 @@
  *  Solo se debe emitir UN valor por pantalla. 
  */
 
+int n = 0;
+int mayor = 0;
+
+for (int i = 1; i < 11; i++)
+{
+    Console.WriteLine("Ingrese numero " + i);
+    n = int.Parse(Console.ReadLine());
+    if (i == 1)
+    {
+        mayor = n;
+    }
+    else
+    {
+        if (n > mayor)
+        {
+            mayor = n;
+        }
+    }
+}
+
+Console.WriteLine("El mayor es " + mayor);
+Console.ReadLine();
