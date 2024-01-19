cb5868bfd19d6776570f011e83266e288e0b7a05
Juan Pablo Formento <juan_pablo94@outlook.com>
2024-01-19 15:08:01 +0100

    ejercicio1 terminado

--- Nivel 1/4-Ciclos/ejercicio1/Program.cs
+++ Nivel 1/4-Ciclos/ejercicio1/Program.cs
@@ -2,3 +2,9 @@
  * Hacer un programa para mostrar los números del 1 al 10. No se debe realizar ningún pedido de datos.
  */
 
+for (int i = 1; i < 11; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.ReadLine();
