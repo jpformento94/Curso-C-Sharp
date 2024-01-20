ae449fcd6e0797bcb5461bc46f15841d1b1fd4c8
Juan Pablo Formento <juan_pablo94@outlook.com>
2024-01-20 15:59:40 +0100

    Ejercicios Ciclos combinados

---
+++ Nivel 1/5-Ciclos combinados/ejercicio1/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * Hacer un programa para ingresar 10 números. El mismo debe analizar y mostrar por pantalla 
+ * cuántos de esos números son primos.
+ */
+
+int n = 0;
+
+for (int i = 1; i < 11; i++)
+{
+    Console.WriteLine("Ingrese numero" + i);
+    n = int.Parse(Console.ReadLine());
+}
+
