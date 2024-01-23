ab4792e7da27e9ae7afd6f373c0cd59cb9df54e6
Juan Pablo Formento <juan_pablo94@outlook.com>
2024-01-23 09:59:28 +0100

    ejercicio 5.1 terminador

--- Nivel 1/5-Ciclos combinados/ejercicio1/Program.cs
+++ Nivel 1/5-Ciclos combinados/ejercicio1/Program.cs
@@ -4,10 +4,27 @@
  */
 
 int n = 0;
+int c = 0;
 
 for (int i = 1; i < 11; i++)
 {
     Console.WriteLine("Ingrese numero" + i);
     n = int.Parse(Console.ReadLine());
+
+    for (int j = 1; j <= n; j++)
+    {
+        if ((n % j) == 0)
+        {
+            c++;
+        }
+    }
+
+    if (c == 2)
+    {
+        Console.WriteLine("El numero " + n + " es primo.");
+    }
+
+    c = 0;
 }
 
+Console.ReadLine();
