11f32f97902b1215601047f39355f6eef7681776
Juan Pablo Formento <juan_pablo94@outlook.com>
2024-01-23 11:18:39 +0100

    ejercicio 5.3 terminado

--- Nivel 1/5-Ciclos combinados/ejercicio3/Program.cs
+++ Nivel 1/5-Ciclos combinados/ejercicio3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Hacer un programa para recibir listas de números positivos que están separadas entre sí por un cero. 
+ * El fin de la carga se notifica con un número negativo. 
+ * Luego mostrar cuántos números tiene cada lista.
+ */
+
+int n, c;
+int numero_grupo = 0;
+
+do
+{
+    numero_grupo++;
+    c = 0;
+    Console.WriteLine("Ingrese un numero para cargar en la lista");
+    Console.WriteLine("Ingrese 0 para terminar la carga de la lista");
+    n = int.Parse(Console.ReadLine());
+    while (n > 0)
+    {
+        c++;
+        n = int.Parse(Console.ReadLine());
+    }
+    Console.WriteLine("La cantidad del grupo " + numero_grupo + " es " + c);
+
+} while (n >= 0);
