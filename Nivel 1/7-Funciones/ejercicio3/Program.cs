d7da1cf12151ad65d8162c6e710a5b82c76df088
Juan Pablo Formento <juan_pablo94@outlook.com>
2024-01-25 22:32:15 +0100

    ejercicio 7.3 terminado

--- Nivel 1/7-Funciones/ejercicio3/Program.cs
+++ Nivel 1/7-Funciones/ejercicio3/Program.cs
@@ -5,8 +5,60 @@
  * sólo los números primos.
  */
 
+
+using System.ComponentModel;
+
+int[] numeros = new int[20];
+
+
 static int primo(int n)
 {
     int flag = 0;
-    if ()
+    int contador = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if ((n % i) == 0)
+        {
+            contador++;
+        }
+    }
+
+    if (contador == 2)
+    {
+        flag = 1;
+    }
+    return flag;
 }
+
+static float promedioPrimos(int[] numeros)
+{
+    int contador = 0;
+    int acumulador = 0;
+    float promedio = 0;
+
+    for (int i = 0; i < numeros.Length; i++)
+    {
+        if (primo(numeros[i]) == 1)
+        {
+            acumulador += numeros[i];
+            contador++;
+        }
+    }
+    return acumulador / contador;
+}
+
+static void cargarVector(ref int[] numeros)
+{
+    Random random = new Random();
+    Console.WriteLine("Elementos del arreglo");
+    for (int i = 0; i < numeros.Length; i++)
+    {
+        numeros[i] = random.Next(0, 99);
+        Console.WriteLine(numeros[i]);
+    }
+}
+
+cargarVector(ref numeros);
+Console.WriteLine("El promedio de los nunmeros primos es " + promedioPrimos(numeros));
+Console.ReadLine();
