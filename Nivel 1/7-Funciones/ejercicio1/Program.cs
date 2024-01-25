dfb2636cd6c206cea616553cfb981f25b67605f0
Juan Pablo Formento <juan_pablo94@outlook.com>
2024-01-25 21:19:21 +0100

    ejercicio 7.1 terminado

---
+++ Nivel 1/7-Funciones/ejercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Hacer una función llamada “producto” que reciba dos números enteros y que devuelva el producto de ambos. 
+ * Luego hacer un programa que pida el precio de un artículo y la cantidad vendida y muestre por pantalla 
+ * el monto total a pagar. Usar la función.
+ */
+
+static int producto(int a, int b)
+{
+    return a * b;
+}
+
+static void calcularVenta()
+{
+    Console.WriteLine("Ingrese el precio del producto");
+    int precio = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese la cantidad vendida");
+    int cantidad = int.Parse(Console.ReadLine());
+    Console.WriteLine("El monto a pagar es " + producto(precio, cantidad));
+}
+
+calcularVenta();
+Console.ReadLine();
