fd021648b6a21f554ca919c2ee372d76ab37b5ba
Juan Pablo Formento <juan_pablo94@outlook.com>
2024-01-25 22:16:06 +0100

    update

---
+++ Nivel 1/7-Funciones/ejercicio3/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+ * Hacer una función llamada “primo” que reciba un número entero y devuelva 1 si el número es primo o 
+ * cero si no lo es. Hacer un programa para ingresar números. 
+ * El lote corta cuando se ingresa un número cero. Informar el promedio teniendo en cuenta 
+ * sólo los números primos.
+ */
+
+static int primo(int n)
+{
+    int flag = 0;
+    if ()
+}
