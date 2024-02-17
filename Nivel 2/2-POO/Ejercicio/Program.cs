ca7f3e207ea1fea4571402721e4ff37219c7046a
Juan Pablo Formento <juan_pablo94@outlook.com>
2024-02-17 03:31:02 +0100

    ejercicio 2 terminado

--- Nivel 2/2-POO/Ejercicio/Program.cs
+++ Nivel 2/2-POO/Ejercicio/Program.cs
@@ -10,18 +10,26 @@ namespace Ejercicio
     {
         static void Main(string[] args)
         {
-            Telefono tel = new Telefono();
-            Console.WriteLine("La marca es " + tel.marca);
-            Console.WriteLine("El modelo es " + tel.modelo);
-            Console.WriteLine("El numero es " + tel.numeroTelefonico);
-            Console.WriteLine("El codigo de operador es " + tel.codigoOperador);
-            tel.numeroTelefonico = "1234";
-            Console.WriteLine("El numero es " + tel.numeroTelefonico);
-            tel.codigoOperador = 123;
-            Console.WriteLine("El codigo de operador es " + tel.codigoOperador);
-            tel.codigoOperador = 1;
-            Console.WriteLine("El codigo de operador es " + tel.codigoOperador);
-            Console.ReadLine();
+            //Telefono 1
+            Telefono telefono1 = new Telefono("Samsung","S24");
+            Console.WriteLine("La marca es " + telefono1.marca);
+            Console.WriteLine("El modelo es " + telefono1.modelo);
+            telefono1.numeroTelefonico = "1234";
+            Console.WriteLine("El numero es " + telefono1.numeroTelefonico);
+            telefono1.codigoOperador = 1;
+            Console.WriteLine("El codigo de operador es " + telefono1.codigoOperador);
+            Console.WriteLine(telefono1.llamar());
+            Console.WriteLine("");
+
+            //Telefono 2
+            Telefono telefono2 = new Telefono("Apple", "16");
+            Console.WriteLine("La marca es " + telefono2.marca);
+            Console.WriteLine("El modelo es " + telefono2.modelo);
+            telefono2.numeroTelefonico = "4321";
+            Console.WriteLine("El numero es " + telefono2.numeroTelefonico);
+            telefono2.codigoOperador = 3;
+            Console.WriteLine("El codigo de operador es " + telefono2.codigoOperador);
+            Console.WriteLine(telefono2.llamar("Juan"));
         }
     }
 }
--- Nivel 2/2-POO/Ejercicio/Telefono.cs
+++ Nivel 2/2-POO/Ejercicio/Telefono.cs
@@ -9,6 +9,12 @@ namespace Ejercicio
     internal class Telefono
     {
 
+        public Telefono(String marca, String modelo)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+        }
+
         public String modelo { get; }
         public String marca { get; }
 
@@ -33,22 +39,14 @@ namespace Ejercicio
             }
         }
 
+        public String llamar()
+        {
+            return "Realizando llamada...";
+        }
 
-
-        /*
-           
-        Agregar Constructor que reciba Modelo y Marca.
-            
-        Crear algunos objetos en el main de Program y probar escribirle datos y mostrar en pantalla el estado del Telefono.
-            
-        Agregar método Llamar() sin parámetros que devuelva un string con la leyenda "Realizando llamada...".
-            
-        Sobrecargar el método Llamar(string contacto) para que reciba un contacto y devuelva un string con la leyenda "Llamando a " + contacto
-            
-        Probar métodos en el main mostrando en pantalla el comportamiento de los objetos.*/
-
-
-
-
+        public String llamar(String contacto)
+        {
+            return $"Llamando a {contacto}";
+        }
     }
 }
