375ba5a7c3e29a12b989c5e2330d482b34550957
Juan Pablo Formento <juan_pablo94@outlook.com>
2024-02-17 03:04:29 +0100

    POO-ejercicio

---
+++ Nivel 2/2-POO/Ejercicio/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Telefono tel = new Telefono();
+            Console.WriteLine("La marca es " + tel.marca);
+            Console.WriteLine("El modelo es " + tel.modelo);
+            Console.WriteLine("El numero es " + tel.numeroTelefonico);
+            Console.WriteLine("El codigo de operador es " + tel.codigoOperador);
+            tel.numeroTelefonico = "1234";
+            Console.WriteLine("El numero es " + tel.numeroTelefonico);
+            tel.codigoOperador = 123;
+            Console.WriteLine("El codigo de operador es " + tel.codigoOperador);
+            tel.codigoOperador = 1;
+            Console.WriteLine("El codigo de operador es " + tel.codigoOperador);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Nivel 2/2-POO/Ejercicio/Telefono.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+{
+    internal class Telefono
+    {
+
+        public String modelo { get; }
+        public String marca { get; }
+
+        public String numeroTelefonico { get; set; }
+
+        private int _codigoOperador;
+
+        public int codigoOperador
+        {
+            get { return _codigoOperador; }
+            set
+            {
+                if ((value == 1) || (value == 2) || (value == 3))
+                {
+                    _codigoOperador = value;
+                }
+                else
+                {
+                    _codigoOperador = 0;
+                }
+
+            }
+        }
+
+
+
+        /*
+           
+        Agregar Constructor que reciba Modelo y Marca.
+            
+        Crear algunos objetos en el main de Program y probar escribirle datos y mostrar en pantalla el estado del Telefono.
+            
+        Agregar método Llamar() sin parámetros que devuelva un string con la leyenda "Realizando llamada...".
+            
+        Sobrecargar el método Llamar(string contacto) para que reciba un contacto y devuelva un string con la leyenda "Llamando a " + contacto
+            
+        Probar métodos en el main mostrando en pantalla el comportamiento de los objetos.*/
+
+
+
+
+    }
+}
